e3dbebe635264048afc7f526334227fbc0b201dd
vey1753 <46813965+vey1753@users.noreply.github.com>
2019-01-18 18:27:34 +0700

    Update Incluede.php

--- Incluede.php
+++ Incluede.php
@@ -1,13 +1,5 @@
 <?php
-/**
- * User: jpinkney
- * Date: 9/15/15
- * Time: 2:15 PM
- */
 
-/**
- * This is the file that you are going to include in each of your new projects
- */
 
 require 'TVMaze/TVMaze.php';
 require 'TVMaze/TVProduction.php';
